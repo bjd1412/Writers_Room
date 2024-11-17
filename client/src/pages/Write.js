de114e93a554bf57af9134f7880c5e12832e80c7
Brendan Dempsey <bjd1412@gmail.com>
2024-11-16 23:40:19 -0500

    added edit button for logged in users

--- client/src/pages/Write.js
+++ client/src/pages/Write.js
@@ -1,17 +1,38 @@
-import React from "react";  
+
+
+import React, { useState, useEffect } from 'react';  
 import { Formik, Form, Field, ErrorMessage } from 'formik';  
 import StoryContext from "../Components/StoryContext";  
 import { useContext } from "react";  
-import * as Yup from 'yup';   
+import * as Yup from 'yup';  
+import { useParams } from 'react-router-dom';  
   
 function Write() {  
+  const { id } = useParams();  
   const { handleSubmit } = useContext(StoryContext);  
-  
-  const initialValues = {  
-   image: '', 
+  const [initialValues, setInitialValues] = useState({  
+   image: '',  
    title: '',  
    story: '',  
-  };  
+  });  
+  const [isLoaded, setIsLoaded] = useState(false);  
+  
+  useEffect(() => {  
+   if (id) {  
+    fetch(`/stories/${id}`)  
+      .then((response) => response.json())  
+      .then((story) => {  
+       setInitialValues({  
+        image: story.image,  
+        title: story.title,  
+        story: story.story,  
+       });  
+       setIsLoaded(true);  
+      });  
+   } else {  
+    setIsLoaded(true);  
+   }  
+  }, [id]);  
   
   const validationSchema = Yup.object().shape({  
    title: Yup.string().required('Title is required'),  
@@ -19,32 +40,44 @@ function Write() {
   });  
   
   const onSubmit = (values, { setSubmitting }) => {  
-   const formData = new FormData();
+   const formData = new FormData();  
    formData.append('image', values.image);  
    formData.append('title', values.title);  
    formData.append('story', values.story);  
   
-   fetch("/stories", {  
-    method: "POST",  
-    body: formData,  
-   })  
-    .then(res => {  
-      console.log('response:', res);  
-      if (res.ok) {  
-       return res.json();  
-      } else {  
-       throw new Error(res.statusText);  
-      }  
+   if (id) {  
+    fetch(`/stories/${id}`, {  
+      method: 'PATCH',  
+      body: formData,  
     })  
-    .then(newWrite => {  
-      console.log('newWrite:', newWrite);  
-      handleSubmit(newWrite);  
+      .then((response) => response.json())  
+      .then((data) => {  
+       setSubmitting(false);  
+      })  
+      .catch((error) => {  
+       console.error(error);  
+       setSubmitting(false);  
+      });  
+   } else {  
+    fetch('/stories', {  
+      method: 'POST',  
+      body: formData,  
     })  
-    .catch(error => {  
-      console.error('Error:', error);  
-    });  
-   setSubmitting(false)  
+      .then((response) => response.json())  
+      .then((data) => {  
+       handleSubmit(data);  
+       setSubmitting(false);  
+      })  
+      .catch((error) => {  
+       console.error(error);  
+       setSubmitting(false);  
+      });  
+   }  
   };  
+  
+  if (!isLoaded) {  
+   return <div>Loading...</div>;  
+  }  
   
   return (  
    <div className="SubmitForm">  
@@ -55,22 +88,22 @@ function Write() {
       onSubmit={onSubmit}  
     >  
       {({ isSubmitting }) => (  
-       <Form className="NewWrite">
-        <label className="Label">Add Image</label>
-        <Field type="text" name="image" placeholder="Add Image"/>
-        <ErrorMessage name="image" component="div"/>  
+       <Form className="NewWrite">  
+        <label className="Label">Add Image</label>  
+        <Field type="text" name="image" placeholder="Add Image" />  
+        <ErrorMessage name="image" component="div" />  
         <label className="Label">Title</label>  
         <Field type="text" name="title" placeholder="Title..." />  
         <ErrorMessage name="title" component="div" />  
         <label className="Label">Story</label>  
-        <Field type="text" name="story" />  
+        <Field type="textarea" name="story" />  
         <ErrorMessage name="story" component="div" />  
-        <input type="submit" value="Submit" disabled={isSubmitting} />  
+        <input type="submit" value={id ? 'Save Changes' : 'Submit'} disabled={isSubmitting} />  
        </Form>  
       )}  
     </Formik>  
    </div>  
-  )  
+  );  
 }  
   
 export default Write;
